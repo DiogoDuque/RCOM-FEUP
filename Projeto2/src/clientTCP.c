4442b65ac0c88e4a33eaeae6e20d2bf5b98de79d
ddw <diogoduque13@hotmail.com>
2016-12-08 12:22:43 +0000

    writes to server with no errors!!! also has example address in a comment in line 32

--- Projeto2/src/clientTCP.c
+++ Projeto2/src/clientTCP.c
@@ -11,30 +11,13 @@
 #include <netdb.h>
 #include <strings.h>
 
-#define SERVER_PORT 6000
-#define SERVER_ADDR "192.168.28.96"
+#define SERVER_PORT 21
 
-char* getIP(char* host) {
+
+char* getIP(char* hostname) {
+
 	struct hostent *h;
-        
-	char hostname[128];
-	if(!strcmp(host,"default")){
-		printf("Using this computer's hostname. (Was intentional, right?)\n");
-		gethostname(hostname, sizeof hostname);
-	} else strcpy(hostname,host);
-        
-/*
-struct hostent {
-	char    *h_name;	Official name of the host. 
-    	char    **h_aliases;	A NULL-terminated array of alternate names for the host. 
-	int     h_addrtype;	The type of address being returned; usually AF_INET.
-    	int     h_length;	The length of the address in bytes.
-	char    **h_addr_list;	A zero-terminated array of network addresses for the host. 
-				Host addresses are in Network Byte Order. 
-};
 
-#define h_addr h_addr_list[0]	The first address in h_addr_list. 
-*/
     if((h=gethostbyname(hostname)) == NULL) {  
         herror("gethostbyname");
         exit(1);
@@ -45,24 +28,54 @@ struct hostent {
     return inet_ntoa(*((struct in_addr *)h->h_addr));
 }
 
-int main(int argc, char** argv){
+
+int main(int argc, char** argv){ // ftp://ftp.up.pt/pub/robots.txt
+
+	if(argc != 2){
+		perror("Usage: ftp ftp://[<user>:<password>@]<host>/<url-path>");
+		exit(0);
+	}
+
 
 	int	sockfd;
 	struct sockaddr_in server_addr;
 	char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
 	int	bytes;
+	char hostname[128], path[128], user[128], pass[128];
+
+
+	/*get info from url (with reg expr)*/
+	if(sscanf(argv[1], "ftp://%[^:]:%[^@]@%[^/]/%s\n", user, pass, hostname, path) == 4){
+		printf("Host: %s\n", hostname);
+		printf("Path: %s\n", path);
+		printf("User: %s\n", user);
+		printf("Pass: %s\n", pass);
+		}else if(sscanf(argv[1], "ftp://%[^/]/%s\n", hostname, path) == 2){
+		printf("Host: %s\n", hostname);
+		printf("Path: %s\n", path);
+		strcpy(user, "Anonymous");
+		strcpy(pass, "password");
+	}else {
+		perror("Invalid URL! Usage: ftp ftp://[<user>:<password>@]<host>/<url-path>");
+		exit(0);
+	}
+	char* SERVER_ADDR = getIP(hostname);
 	
+
 	/*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
     
+
 	/*open a TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     		perror("socket()");
         	exit(0);
    	}
+
+
 	/*connect to the server*/
    	if(connect(sockfd, 
            (struct sockaddr *)&server_addr, 
@@ -70,6 +83,7 @@ int main(int argc, char** argv){
        	perror("connect()");
 		exit(0);
 	}
+
 
    	/*send a string to the server*/
 	bytes = write(sockfd, buf, strlen(buf));
