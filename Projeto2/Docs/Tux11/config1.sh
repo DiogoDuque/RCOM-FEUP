fcf9a9bcdbc28937217bae52f5ffc8ed632f0197
fariagu <gustavoduartefaria@gmail.com>
2016-12-09 11:41:35 +0000

    /etc/init.d/networking restart

--- Projeto2/Docs/Tux11/config1.sh
+++ Projeto2/Docs/Tux11/config1.sh
@@ -1,4 +1,7 @@
+/etc/init.d/networking restart
+#ifconfig eth0 up
 ifconfig eth0 172.16.10.1/24
+
 route add default gw 172.16.10.254
 route add -net 172.16.10.0/24 gw 0.0.0.0
 route add -net 172.16.11.0/24 gw 176.16.10.254
--- Projeto2/Docs/Tux14/config4.sh
+++ Projeto2/Docs/Tux14/config4.sh
@@ -1,5 +1,8 @@
+#ifconfig eth0 up
 ifconfig eth0 172.16.10.254/24
+#ifconfig eth1 up
 ifconfig eth1 172.16.11.253/24
+
 route add default gw 172.16.11.254
 route add -net 172.16.10.0/24 gw 176.16.10.254
 route add -net 172.16.10.0/24 gw 0.0.0.0
