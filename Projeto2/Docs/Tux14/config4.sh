d7525232b88523767af90f62667ba6ee49e09f6c
DiogoDuque <diogoduque13@hotmail.com>
2016-12-09 14:36:50 +0000

    scripts are now 100% correct! also added useful info to info.txt

--- Projeto2/Docs/Tux14/config4.sh
+++ Projeto2/Docs/Tux14/config4.sh
@@ -1,10 +1,9 @@
-#ifconfig eth0 up
+/etc/init.d/networking restart
 ifconfig eth0 172.16.10.254/24
-#ifconfig eth1 up
 ifconfig eth1 172.16.11.253/24
 
 route add default gw 172.16.11.254
-route add -net 172.16.10.0/24 gw 176.16.10.254
 route add -net 172.16.10.0/24 gw 0.0.0.0
-route add -net 172.16.11.0/24 gw 176.16.11.253
 route add -net 172.16.11.0/24 gw 0.0.0.0
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
